685ce20b17ff88fde8c7a297ec0cb20ce2292ad7
starcalm96 <xingguang96>
2014-06-04 00:10:00 +0800

    user_info.php & login.php
    user_info.php做好了
    login的时候没有错误提示

--- user_info.php
+++ user_info.php
@@ -1,3 +1,5 @@
+<!-- 空输入没有报错 -->
+
 <?php session_start();?>
 <html lang="en">
 
@@ -30,17 +32,36 @@ if(mysqli_num_rows($data)==1){
 <script type="text/javascript" src="js/jquery.min.js"></script>
 <script type="text/javascript">
 $(document).ready(function(){
-  $("button").click(function(){
-    $("form#9").replaceWith("<form role='form' action='user_info.php' method='post' class='form-horizontal'><div class='form-group' ><label class='col-sm-2 control-label'>User ID</label><div class='col-sm-4'><input class='form-control' id='disabledInput' type='text' placeholder='<?php echo $_SESSION['user_id'];?>' disabled></div></div><div class='form-group' ><label class='col-sm-2 control-label'>User Name</label><div class='col-sm-4'><input class='form-control' id='disabledInput' type='text' placeholder='<?php echo $_SESSION['username'];?>' disabled></div></div><div class='form-group' ><label class='col-sm-2 control-label'>Enroll Time</label><div class='col-sm-4'><input class='form-control' id='disabledInput' type='text' placeholder='<?php echo $_SESSION['department'];?>' disabled></div></div><div class='form-group' ><label class='col-sm-2 control-label'>Department</label><div class='col-sm-4'><input class='form-control' id='disabledInput' type='text' placeholder='<?php echo $_SESSION['gender'];?>' disabled></div></div><div class='form-group' ><label class='col-sm-2 control-label'>Gender</label><div class='col-sm-4'><input class='form-control' id='disabledInput' type='text' placeholder='<?php echo $_SESSION['birthyear'].'-'.$_SESSION['birthmonth'].'-'.$_SESSION['birthday'];?>'disabled></div></div><div class='form-group' ><label class='col-sm-2 control-label'>Birth Day</label><div class='col-sm-4'><input class='form-control' id='disabledInput' type='text' placeholder='<?php echo $_SESSION['enroll_time'];?>' disabled></div></div><div class='form-group'><label class='col-sm-2 control-label'>Phone</label><div class='col-sm-4'><input type='tel' class='form-control' name='phone' id='phonenum' value='<?php echo $_SESSION['phone']?>' required></div></div><div class='form-group'><label class='col-sm-2 control-label'>Email</label><div class='col-sm-4'><input type='email' name='email' class='form-control' id='emailaddr' value='<?php echo $_SESSION['email']?>' required></div></div><div class='form-group'><label class='col-sm-5 control-label'></label><input class='btn btn-primary' name='submit' type='submit' value='Done' id='refresh'></div></form>")
-    $("button#1").replaceWith("")
+  $("button#editBTN").click(function(){
+    $("form#former").hide();
+    $("form#latter").show();
+    $("button#editBTN").hide();
   });
-  $(".alert").click(function(){
-    window.location.reload();
+  $("button#doneBTN").click(function(event){
+    event.preventDefault();
+    var newphone = $("input#phonenum").val();
+    var newemail = $("input#emailaddr").val();
+    $.ajax({
+      type: "POST"
+      url:"user_info.php",
+      data: "phone=" + newphone + "&email=" + newemail,
+      success: function(){
+        $('#phone_label').text(newphone);
+        $('#email_label').text(newemail);
+        $("form#former").show();
+        $("form#latter").hide();
+        $("#success").show();
+        // var msgdiv = $("<div></div>");
+        // msgdiv.addClass("alert alert-success alert-dismissable");
+        // msgdiv.text("Your profile have been updated!");
+        // msgdiv.insertBefore($('#former'));
+      },
+      error: function(){
+        $("#faile").show()
+      }
+    });
   });
 });
-// function refresh(){
-//   window.location.reload();
-// }
 </script>
 </head>
 
@@ -54,13 +75,27 @@ $(document).ready(function(){
 
       <!-- Begin page content -->
       <div class="container">
-        <div class="page-header">
-          <h1>User Infomation <button class="btn btn-sm btn-default" id='1'>Edit</button></h1>
+        <div class="page-header" id="page-header">
+          <h1>User Infomation <button class="btn btn-sm btn-default" id='editBTN'>Edit</button></h1>
+        </div>
+
+        <!-- success prompt -->
+        <div class='alert alert-success alert-dismissable' style="display:none" id="success">
+          <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>×</button>
+          <h4>Success</h4>
+          <strong>Your profile have been updated!</strong></a>
+        </div>
+
+        <!-- failed prompt -->
+        <div class='alert alert-warning alert-dismissable' style="display:none" id="fail">
+          <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>×</button>
+          <h4>Failed!</h4> 
+          <strong>Try again!</strong></a>
         </div>
 
         <!-- 用来与数据库交互 -->
         <?php
-          if(isset($_POST['submit'])){
+          if($_SERVER['REQUEST_METHOD']=="POST"){
             $data = FALSE;
             if($_POST['phone']!=''){
               $phone=$_POST['phone'];
@@ -74,73 +109,138 @@ $(document).ready(function(){
             }
             if ($data == TRUE) {
               $data = mysqli_query($dbc, $query);
-              echo "<div class='alert alert-success alert-dismissable'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>×</button><h4>Success</h4><strong>Your profile have been updated!</strong></a></div>";
+              echo "success";
             }
             else{
-              echo "<div class='alert alert-warning alert-dismissable'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>×</button><h4>Failed!</h4> <strong>Try again!</strong></a></div>";       
+              echo "fail";       
             }
           }
-
         ?>
+
         <!-- page body -->
-        <form class="form-horizontal" id='9'>
+        <!-- former table used to display info of user -->
+        <form class="form-horizontal" id='former'>
+          <div class="form-group" >
+            <label class="col-sm-2 control-label">User ID</label>
+            <div class="col-sm-4">
+              <p class="form-control-static"><?php echo $_SESSION['user_id'];?></p>
+            </div>
+          </div>
+          
+          <div class="form-group">
+            <label class="col-sm-2 control-label">User Name</label>
+            <div class="col-sm-4">
+              <p class="form-control-static"><?php echo $_SESSION['username'];?></p>
+            </div>
+          </div>
+           
+          <div class="form-group" >
+            <label class="col-sm-2 control-label">Department</label>
+            <div class="col-sm-4">
+              <p class="form-control-static"><?php echo $_SESSION['department'];?></p>
+            </div>
+          </div>
+           
+          <div class="form-group">
+            <label class="col-sm-2 control-label">Gender</label>
+            <div class="col-sm-4">
+              <p class="form-control-static"><?php echo $_SESSION['gender'];?></p>
+            </div>
+          </div>
+           
+          <div class="form-group">
+            <label class="col-sm-2 control-label">Birth Day</label>
+            <div class="col-sm-4">
+              <p class="form-control-static"><?php echo $_SESSION['birthyear']."-".$_SESSION['birthmonth']."-".$_SESSION['birthday'];?></p>
+            </div>
+          </div>
+           
+          <div class="form-group">
+            <label class="col-sm-2 control-label">Enroll time</label>
+            <div class="col-sm-4">
+              <p class="form-control-static"><?php echo $_SESSION['enroll_time'];?></p>
+            </div>
+          </div>
 
-        <div class="form-group" >
-          <label class="col-sm-2 control-label">User ID</label>
-          <div class="col-sm-4">
-            <p class="form-control-static" id='0'><?php echo $_SESSION['user_id'];?></p>
+          <div class="form-group">
+            <label class="col-sm-2 control-label">Phone</label>
+            <div class="col-sm-4">
+              <p class="form-control-static" id="phone_label"><?php echo $_SESSION['phone'];?></p>
+            </div>
           </div>
-        </div>
-        
-        <div class="form-group">
-          <label class="col-sm-2 control-label">User Name</label>
-          <div class="col-sm-4">
-            <p class="form-control-static" id="2"><?php echo $_SESSION['username'];?></p>
-          </div>
-        </div>
-         
-        <div class="form-group" >
-          <label class="col-sm-2 control-label">Department</label>
-          <div class="col-sm-4">
-            <p class="form-control-static" id="3"><?php echo $_SESSION['department'];?></p>
-          </div>
-        </div>
-         
-        <div class="form-group">
-          <label class="col-sm-2 control-label">Gender</label>
-          <div class="col-sm-4">
-            <p class="form-control-static" id="4"><?php echo $_SESSION['gender'];?></p>
-          </div>
-        </div>
-         
-        <div class="form-group">
-          <label class="col-sm-2 control-label">Birth Day</label>
-          <div class="col-sm-4">
-            <p class="form-control-static" id="5"><?php echo $_SESSION['birthyear']."-".$_SESSION['birthmonth']."-".$_SESSION['birthday'];?></p>
-          </div>
-        </div>
-         
-        <div class="form-group">
-          <label class="col-sm-2 control-label">Enroll time</label>
-          <div class="col-sm-4">
-            <p class="form-control-static" id="6"><?php echo $_SESSION['enroll_time'];?></p>
-          </div>
-        </div>
 
-        <div class="form-group">
-          <label class="col-sm-2 control-label">Phone</label>
-          <div class="col-sm-4">
-            <p class="form-control-static" id="7"><?php echo $_SESSION['phone'];?></p>
+          <div class="form-group">
+            <label class="col-sm-2 control-label">Email</label>
+            <div class="col-sm-4">
+              <p class="form-control-static" id="email_label"><?php echo $_SESSION['email'];?></p>
+            </div>
           </div>
-        </div>
+        </form>
 
-        <div class="form-group">
-          <label class="col-sm-2 control-label">Email</label>
-          <div class="col-sm-4">
-            <p class="form-control-static" id="8"><?php echo $_SESSION['email'];?></p>
+        <!-- latter form used to edit info -->
+        <form role='form' method='post' class='form-horizontal' id='latter' style="display: none">
+
+          <div class='form-group' >
+            <label class='col-sm-2 control-label'>User ID</label>
+            <div class='col-sm-4'>
+              <input class='form-control' id='disabledInput' type='text' placeholder='<?php echo $_SESSION['user_id'];?>' disabled>
+            </div>
           </div>
-        </div>
-      </form>
+
+          <div class='form-group' >
+            <label class='col-sm-2 control-label'>User Name</label>
+            <div class='col-sm-4'>
+              <input class='form-control' id='disabledInput' type='text' placeholder='<?php echo $_SESSION['username'];?>' disabled>
+            </div>
+          </div>
+
+          <div class='form-group' >
+            <label class='col-sm-2 control-label'>Enroll Time</label>
+            <div class='col-sm-4'>
+              <input class='form-control' id='disabledInput' type='text' placeholder='<?php echo $_SESSION['department'];?>' disabled>
+            </div>
+          </div>
+
+          <div class='form-group' >
+            <label class='col-sm-2 control-label'>Department</label>
+            <div class='col-sm-4'>
+              <input class='form-control' id='disabledInput' type='text' placeholder='<?php echo $_SESSION['gender'];?>' disabled>
+            </div>
+          </div>
+
+          <div class='form-group' >
+            <label class='col-sm-2 control-label'>Gender</label>
+            <div class='col-sm-4'>
+              <input class='form-control' id='disabledInput' type='text' placeholder='<?php echo $_SESSION['birthyear'].'-'.$_SESSION['birthmonth'].'-'.$_SESSION['birthday'];?>'disabled>
+            </div>
+          </div>
+
+          <div class='form-group' >
+            <label class='col-sm-2 control-label'>Birth Day</label>
+            <div class='col-sm-4'>
+              <input class='form-control' id='disabledInput' type='text' placeholder='<?php echo $_SESSION['enroll_time'];?>' disabled>
+            </div>
+          </div>
+
+          <div class='form-group'>
+            <label class='col-sm-2 control-label'>Phone</label>
+            <div class='col-sm-4'>
+              <input type='tel' class='form-control' name='phone' id='phonenum' value='<?php echo $_SESSION['phone']?>' required>
+            </div>
+          </div>
+
+          <div class='form-group'>
+            <label class='col-sm-2 control-label'>Email</label>
+            <div class='col-sm-4'>
+              <input type='email' name='email' class='form-control' id='emailaddr' value='<?php echo $_SESSION['email']?>' required>
+            </div>
+          </div>
+
+          <div class='form-group'>
+            <label class='col-sm-5 control-label'></label>
+            <button class='btn btn-primary' name='submit' type='submit' value='Done' id='doneBTN'>Done</button>
+          </div>
+        </form>
 
       </div>
     </div>
@@ -164,6 +264,6 @@ $(document).ready(function(){
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="http://cdn.bootcss.com/jquery/1.10.2/jquery.min.js"></script>
-    <script src="../../dist/js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
