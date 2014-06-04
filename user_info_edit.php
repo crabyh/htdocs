08652ac230d6fbee977b3801f64878b5e93bc3c7
crabyh <crabyh01@gmail.com>
2014-06-04 20:32:01 +0800

    course/user_info_edit UI

--- user_info_edit.php
+++ user_info_edit.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <!-- checking illegal access -->
-<?php //include 'check_access.php'; ?>
+<?php include 'check_access.php'; ?>
 
 <!-- accessing database -->
 <?php 
@@ -11,17 +11,8 @@ $dbc = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 $user_id = mysqli_real_escape_string($dbc,trim($_SESSION['user_id']));
 $query = "SELECT * FROM user_info WHERE user_id = '$user_id'";
 $data = mysqli_query($dbc,$query) or die ("update accounts erroe");
-if(mysqli_num_rows($data)==1){
+if(mysqli_num_rows($data)==1)
   $row = mysqli_fetch_array($data);
-  $_SESSION['department'] = $row['department'];
-  $_SESSION['gender'] = $row['gender'];
-  $_SESSION['birthyear'] = $row['birthyear'];
-  $_SESSION['birthmonth'] = $row['birthmonth'];
-  $_SESSION['birthday'] = $row['birthday'];
-  $_SESSION['enroll_time'] = $row['enroll_time'];
-  $_SESSION['phone'] = $row['phone'];
-  $_SESSION['email'] = $row['email'];
-  }
 ?>
 
 <!-- include head file-->
@@ -49,74 +40,72 @@ if(mysqli_num_rows($data)==1){
         <div class="form-group">
           <label class="col-sm-2 control-label">User ID</label>
           <div class="col-sm-4">
-            <input class="form-control" id="disabledInput" type="text" placeholder="<?php echo $_SESSION['user_id'];?>" disabled>
+            <input class="form-control" id="disabledInput" type="text" placeholder="<?php echo $row['user_id'];?>" disabled>
           </div>
         </div>
         
         <div class="form-group">
           <label class="col-sm-2 control-label">User Name</label>
           <div class="col-sm-4">
-            <input class="form-control" id="disabledInput" type="text" placeholder="<?php echo $_SESSION['username'];?>" disabled>
+            <input class="form-control" id="disabledInput" type="text" placeholder="<?php echo $row['username'];?>" disabled>
           </div>
         </div>
          
         <div class="form-group">
           <label class="col-sm-2 control-label">Department</label>
           <div class="col-sm-4">
-            <input class="form-control" id="disabledInput" type="text" placeholder="<?php echo $_SESSION['department'];?>" disabled>
+            <input class="form-control" id="disabledInput" type="text" placeholder="<?php echo $row['department'];?>" disabled>
           </div>
         </div>
          
         <div class="form-group">
           <label class="col-sm-2 control-label">Gender</label>
           <div class="col-sm-4">
-            <input class="form-control" id="disabledInput" type="text" placeholder="<?php echo $_SESSION['gender'];?>" disabled>
+            <input class="form-control" id="disabledInput" type="text" placeholder="<?php echo $row['gender'];?>" disabled>
           </div>
         </div>
          
         <div class="form-group">
           <label class="col-sm-2 control-label">Birth Day</label>
           <div class="col-sm-4">
-            <input class="form-control" id="disabledInput" type="text" placeholder="<?php echo $_SESSION['birthyear']."-".$_SESSION['birthmonth']."-".$_SESSION['birthday'];?>"disabled>
+            <input class="form-control" id="disabledInput" type="text" placeholder="<?php echo $row['birthyear']."-".$row['birthmonth']."-".$row['birthday'];?>"disabled>
           </div>
         </div>
          
         <div class="form-group">
           <label class="col-sm-2 control-label">Enroll time</label>
           <div class="col-sm-4">
-            <input class="form-control" id="disabledInput" type="text" placeholder="<?php echo $_SESSION['enroll_time'];?>" disabled>
+            <input class="form-control" id="disabledInput" type="text" placeholder="<?php echo $row['enroll_time'];?>" disabled>
           </div>
         </div>
 
         <div class="form-group">
           <label class="col-sm-2 control-label">Phone</label>
           <div class="col-sm-4">
-            <input type="tel" class="form-control" id="inputPassword" placeholder="<?php echo $_SESSION["phone"]?>">
+            <input type="tel" class="form-control" id="inputPassword" placeholder="<?php echo $row["phone"]?>">
           </div>
         </div>
 
         <div class="form-group">
           <label class="col-sm-2 control-label">Email</label>
           <div class="col-sm-4">
-            <input type="email" class="form-control" id="inputPassword" placeholder="<?php echo $_SESSION["email"]?>">
+            <input type="email" class="form-control" id="inputPassword" placeholder="<?php echo $row["email"]?>">
           </div>
         </div>
+
+		    <div class="form-group">
+          <div class="col-sm-2">
+          </div>
+          <div class="col-sm-6">
+            <button class="btn btn-primary" type="submit" name="submit" value="submit">Submit</button>
+            <input class="btn btn-default" type="button" name="reset" value="reset">
+          </div>
+        </div>
+
       </form>
 
       </div>
     </div>
-
-    <!-- clear user_info Session -->
-    <?php
-      unset($_SESSION['department']);
-      unset($_SESSION['gender']);
-      unset($_SESSION['birthyear']);
-      unset($_SESSION['birthmonth']);
-      unset($_SESSION['birthday']);
-      unset($_SESSION['enroll_time']);
-      unset($_SESSION['phone']);
-      unset($_SESSION['email']);
-    ?>
 
     <!-- page footer -->
     <?php include"footer.php"; ?>
