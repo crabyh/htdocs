5c902de660195f7335dcb464cf2f48562aeb0c41
HyukSoa <Soa@zhangxiaomengdeMacBook-Air.local>
2014-06-12 15:54:31 +0800

    添加了用户头像的显示和上传
    仍待解决：默认头像的问题。

--- upload.php
+++ upload.php
@@ -1,15 +1,95 @@
-<html>
+<?php session_start();?>
+<html lang="en">
+
+<!-- checking illegal access -->
+<?php include 'check_access.php'; ?>
+
+<!-- include head file-->
+<head>
+<?php include 'header.php'; ?>
+</head>
+
 <body>
 
-<form method="POST" action="upload.php" enctype="multipart/form-data">
-<label for="file">Filename:</label>
-  <input type="hidden" name="MAX_FILE_SIZE" value="1024000" />
-  <input type="file" name="file" id="file"/><br />
-  <input type="submit" name ="submit" value="submit" /></p>
-</form>
+    <!-- Wrap all page content here -->
+    <div id="wrap">
 
-</body>
+      <!-- Fixed navbar -->
+      <?php include 'navigation.php'; ?>
+
+      <!-- Begin page content -->
+      <div class="container">
+        <div class="page-header">
+        <?php
+        if(isset($_SESSION['login'])) 
+          echo'<div class="alert alert-success alert-dismissable">
+           <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+          <h4>
+            Sign Out
+          </h4> <strong>You have been sign out successful.</strong></a>
+        </div>';
+        else if(isset($_SESSION['illegal']))
+          echo'<div class="alert alert-danger fade in"">
+           <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+          <h4>
+            Illegal Access
+          </h4> <strong>xxxxxxx.</strong></a>
+        </div>';
+        unset($_SESSION['login']);
+        unset($_SESSION['illegal']);
+        ?>
+          <h1>upload</h1>
+        </div>
+
+        <!-- page body -->
+        <!-- <p class="lead">Enjoy the system. Have fun!</p> -->
+        <!--open system log file and print it -->
+
+
+        <!--上面这段php就是输出系统日志文件的整个结构，如果要美化的话在外面加个框吧 by女王-->
+<button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title" id="myModalLabel">Upload Icon</h4>
+      </div><!-- 里面的内容 -->
+
+      <div class="modal-body">
+        <p2>Choose a picture from your computer to upload as your icon.<br />
+            Pay attention that you could only upload <b>jpg</b> fileand the size of your file couldn't be lager than <b>20kb</b>.<br /><br /></p>
+        <form method="POST" action="upload.php" enctype="multipart/form-data">
+          <input type="hidden" name="MAX_FILE_SIZE" value="1024000" />
+          <input type="file" name="file" id="file"/><br />
+          <input class="btn btn-primary" type="submit" value="Upload" /></p>
+        </form>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+      </div>
+    </div>
+
+
+
+
+    <!-- page footer -->
+    <?php include"footer.php"; ?>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="http://cdn.bootcss.com/jquery/1.10.2/jquery.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+  </body>
 </html>
+
 
 
 
@@ -17,8 +97,8 @@
 <?php 
 if(isset($_FILES["file"]))
 {
-  if ((($_FILES["file"]["type"] == "image/gif")
-  || ($_FILES["file"]["type"] == "image/jpeg")
+  if ((//($_FILES["file"]["type"] == "image/png")||
+     ($_FILES["file"]["type"] == "image/jpeg")
   || ($_FILES["file"]["type"] == "image/pjpeg")
   )
   && ($_FILES["file"]["size"] < 2000000))
@@ -29,67 +109,28 @@ if(isset($_FILES["file"]))
       }
     else
       {
+      $iconname = $_SESSION['user_id'] . ".jpg";
+      move_uploaded_file($_FILES["file"]["tmp_name"],"uploadicon/" . $iconname);
+      echo "Stored in: " . "uploadicon/" . $iconname;          
+
+      //输出文件信息 测试用
       echo "Upload: " . $_FILES["file"]["name"] . "<br />";
       echo "Type: " . $_FILES["file"]["type"] . "<br />";
       echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-      echo "Stored in: " . $_FILES["file"]["tmp_name"];
+      echo "Stored in: " . $_FILES["file"]["tmp_name"] . "<br />";
+      //输出文件信息结束
 
-      if (file_exists("upload/" . $_FILES["file"]["name"]))
-      {
-      echo $_FILES["file"]["name"] . " already exists. ";
-      }
-      else
-      {
-      move_uploaded_file($_FILES["file"]["tmp_name"],
-      "upload/" . $_FILES["file"]["name"]);
-      echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
-      }
+//      if (file_exists("upload/" . $_FILES["file"]["name"]))
+//      {
+//      echo $_FILES["file"]["name"] . " already exists. ";
+//      }
+//      else
+//      {
+
+//      }
       }
     }
     else
       echo "file type or size wrong.";
 }
 ?>
-
-<!--
-<?php
-if(isset($_FILES["file"]))
-{
-if ((($_FILES["file"]["type"] == "image/gif")
-|| ($_FILES["file"]["type"] == "image/jpeg")
-|| ($_FILES["file"]["type"] == "image/pjpeg")
-|| ($_FILES["file"]["type"] == "h")
-)
-&& ($_FILES["file"]["size"] < 20000))
-  {
-  if ($_FILES["file"]["error"] > 0)
-    {
-    echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
-    }
-  else
-    {
-    echo "Upload: " . $_FILES["file"]["name"] . "<br />";
-    echo "Type: " . $_FILES["file"]["type"] . "<br />";
-    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
-
-    if (file_exists("upload/" . $_FILES["file"]["name"]))
-      {
-      echo $_FILES["file"]["name"] . " already exists. ";
-      }
-    else
-      {
-      move_uploaded_file($_FILES["file"]["tmp_name"],
-      "upload/" . $_FILES["file"]["name"]);
-      echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
-      }
-    }
-    echo "upload succeed!";
-  }
-else
-  {
-  echo "Invalid file";
-  }
-}
-?>
--->
--- user_info.php
+++ user_info.php
@@ -16,6 +16,33 @@ if(mysqli_num_rows($data)==1){
 }
 ?>
 
+<!--上传头像的php部分 by 女王-->
+<?php 
+if(isset($_FILES["file"]))
+{
+  if ((//($_FILES["file"]["type"] == "image/png")||
+     ($_FILES["file"]["type"] == "image/jpeg")
+  || ($_FILES["file"]["type"] == "image/pjpeg")
+  )
+  && ($_FILES["file"]["size"] < 2000000))
+    {
+    if ($_FILES["file"]["error"] > 0)
+      {
+      echo "Error: " . $_FILES["file"]["error"] . "<br />";//上传错误
+      }
+    else
+      {
+      $iconname = $_SESSION['user_id'] . ".jpg";
+      move_uploaded_file($_FILES["file"]["tmp_name"],"uploadicon/" . $iconname);
+      echo "Stored in: " . "uploadicon/" . $iconname;          
+      }
+    }
+    else
+      echo "file type or size wrong.";
+}
+?>
+
+
 <!-- include head file-->
 <head>
 <?php include 'header.php'; ?>
@@ -63,7 +90,29 @@ $(document).ready(function(){
       <!-- Begin page content -->
       <div class="container">
         <div class="page-header" id="page-header">
-          <h1>User Infomation <button class="btn btn-sm btn-default" id='editBTN'>Edit</button></h1>
+          <h1>User Infomation <button class="btn btn-sm btn-default" id='editBTN'>EditTxt</button>
+          <button class="btn btn-sm btn-default" id='uploadicon' data-toggle="modal" data-target="#myModal">UploadIcon</button></h1>     <!-- data-tog 到myModal均为下拉菜单的代码部分-->    
+          <!-- Modal -->
+          <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                  <h4 class="modal-title" id="myModalLabel">Upload Icon</h4>
+                </div><!-- 里面的内容 -->
+
+                <div class="modal-body">
+                  <p>Choose a picture from your computer to upload as your icon.<br />
+                  Pay attention that you could only upload <b>jpg</b> file ,and the size of your file couldn't be lager than <b>20kb</b> and <b>140x140</b>.<br /><br /></p>
+                  <form method="POST" action="user_info.php" enctype="multipart/form-data">
+                    <input type="hidden" name="MAX_FILE_SIZE" value="1024000" />
+                    <input type="file" name="file" id="file"/><br />
+                    <input class="btn btn-primary" type="submit" value="Upload" /></p>
+                  </form>
+                </div>
+              </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+          </div><!-- /.modal -->
         </div>
 
         <!-- success prompt -->
@@ -80,9 +129,9 @@ $(document).ready(function(){
           <strong>Try again!</strong></a>
         </div>
 
-        <!-- 用来与数据库交互 -->
+        <!-- 用来与数据库交互 --> <!-- 因为上传文件的提交方式也是post，所以我在下面的if判断句中加了一个判断条件isset($_POST['phone']) by女王 -->
         <?php
-          if($_SERVER['REQUEST_METHOD']=="POST"){ 
+          if($_SERVER['REQUEST_METHOD']=="POST" and isset($_POST['phone'])){ 
             $data = FALSE;
             if($_POST['phone']!=''){
               $phone=$_POST['phone'];
@@ -106,131 +155,144 @@ $(document).ready(function(){
 
         <!-- page body -->
         <!-- former table used to display info of user -->
-        <form class="form-horizontal" id='former'>
+        <!-- 前端显示的class原来为col-sm-格式，被我统一调整成了col-md-4格式。同时添加进了row featurette，允许头像与个人信息并排显示。by女王 -->
+    <form class="form-horizontal" id='former'>
+      <div class="row featurette">
+       <div class="col-md-8">
+
           <div class="form-group" >
-            <label class="col-sm-2 control-label">User ID</label>
-            <div class="col-sm-4">
+            <label class="col-md-4 control-label">User ID</label>
+            <div class="col-md-4">
               <p class="form-control-static"><?php echo $row['user_id'];?></p>
             </div>
           </div>
           
           <div class="form-group">
-            <label class="col-sm-2 control-label">User Name</label>
-            <div class="col-sm-4">
+            <label class="col-md-4 control-label">User Name</label>
+            <div class="col-md-4">
               <p class="form-control-static"><?php echo $row['username'];?></p>
             </div>
           </div>
            
           <div class="form-group" >
-            <label class="col-sm-2 control-label">Department</label>
-            <div class="col-sm-4">
+            <label class="col-md-4 control-label">Department</label>
+            <div class="col-md-4">
               <p class="form-control-static"><?php echo $row['department'];?></p>
             </div>
           </div>
            
           <div class="form-group">
-            <label class="col-sm-2 control-label">Gender</label>
-            <div class="col-sm-4">
+            <label class="col-md-4 control-label">Gender</label>
+            <div class="col-md-4">
               <p class="form-control-static"><?php echo $row['gender'];?></p>
             </div>
           </div>
            
           <div class="form-group">
-            <label class="col-sm-2 control-label">Birth Day</label>
-            <div class="col-sm-4">
+            <label class="col-md-4 control-label">Birth Day</label>
+            <div class="col-md-4">
               <p class="form-control-static"><?php echo $row['birthyear']."-".$row['birthmonth']."-".$row['birthday'];?></p>
             </div>
           </div>
            
           <div class="form-group">
-            <label class="col-sm-2 control-label">Enroll time</label>
-            <div class="col-sm-4">
+            <label class="col-md-4 control-label">Enroll time</label>
+            <div class="col-md-4">
               <p class="form-control-static"><?php echo $row['enroll_time'];?></p>
             </div>
           </div>
 
           <div class="form-group">
-            <label class="col-sm-2 control-label">Phone</label>
-            <div class="col-sm-4">
+            <label class="col-md-4 control-label">Phone</label>
+            <div class="col-md-4">
               <p class="form-control-static" id="phone_label"><?php echo $row['phone'];?></p>
             </div>
           </div>
 
           <div class="form-group">
-            <label class="col-sm-2 control-label">Email</label>
-            <div class="col-sm-4">
+            <label class="col-md-4 control-label">Email</label>
+            <div class="col-md-4">
               <p class="form-control-static" id="email_label"><?php echo $row['email'];?></p>
             </div>
           </div>
         </form>
+      </div>
+      <!--即显示头像的语句-->
+      <div class="col-md-4">
+        <img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="140x140" src="uploadicon/<?php echo$_SESSION['user_id'];?>.jpg">
+      </div>
+    </div>
 
         <!-- latter form used to edit info -->
-        <form role='form' method='post' class='form-horizontal' id='latter' style="display: none">
-
+    <form role='form' method='post' class='form-horizontal' id='latter' style="display: none">
+    <div class="row featurette">
+      <div class="col-md-9">
           <div class='form-group' >
-            <label class='col-sm-2 control-label'>User ID</label>
-            <div class='col-sm-4'>
+            <label class='col-md-4 control-label'>User ID</label>
+            <div class='col-md-4'>
               <input class='form-control' type='text' placeholder='<?php echo $row['user_id'];?>' disabled>
             </div>
           </div>
 
           <div class='form-group' >
-            <label class='col-sm-2 control-label'>User Name</label>
-            <div class='col-sm-4'>
+            <label class='col-md-4 control-label'>User Name</label>
+            <div class='col-md-4'>
               <input class='form-control' type='text' placeholder='<?php echo $row['username'];?>' disabled>
             </div>
           </div>
 
           <div class='form-group' >
-            <label class='col-sm-2 control-label'>Enroll Time</label>
-            <div class='col-sm-4'>
+            <label class='col-md-4 control-label'>Enroll Time</label>
+            <div class='col-md-4'>
               <input class='form-control' type='text' placeholder='<?php echo $row['department'];?>' disabled>
             </div>
           </div>
 
           <div class='form-group' >
-            <label class='col-sm-2 control-label'>Department</label>
-            <div class='col-sm-4'>
+            <label class='col-md-4 control-label'>Department</label>
+            <div class='col-md-4'>
               <input class='form-control' type='text' placeholder='<?php echo $row['gender'];?>' disabled>
             </div>
 
           </div>
 
           <div class='form-group' >
-            <label class='col-sm-2 control-label'>Gender</label>
-            <div class='col-sm-4'>
+            <label class='col-md-4 control-label'>Gender</label>
+            <div class='col-md-4'>
               <input class='form-control' type='text' placeholder='<?php echo $row['birthyear'].'-'.$row['birthmonth'].'-'.$row['birthday'];?>'disabled>
             </div>
 
           </div>
 
           <div class='form-group' >
-            <label class='col-sm-2 control-label'>Birth Day</label>
-            <div class='col-sm-4'>
+            <label class='col-md-4 control-label'>Birth Day</label>
+            <div class='col-md-4'>
               <input class='form-control' type='text' placeholder='<?php echo $row['enroll_time'];?>' disabled>
             </div>
 
           </div>
 
           <div class='form-group'>
-            <label class='col-sm-2 control-label'>Phone</label>
-            <div class='col-sm-4'>
+            <label class='col-md-4 control-label'>Phone</label>
+            <div class='col-md-4'>
               <input type='tel' class='form-control' name='phone' id='phonenum' value='<?php echo $row['phone']?>' required>
             </div>
           </div>
 
           <div class='form-group'>
-            <label class='col-sm-2 control-label'>Email</label>
-            <div class='col-sm-4'>
+            <label class='col-md-4 control-label'>Email</label>
+            <div class='col-md-4'>
               <input type='email' name='email' class='form-control' id='emailaddr' value='<?php echo $row['email']?>' required>
             </div>
           </div>
 
           <div class='form-group'>
-            <label class='col-sm-5 control-label'></label>
+            <label class='col-sm-6 control-label'></label>
             <button class='btn btn-primary' name='submit' type='submit' value='Done' id='doneBTN'>Done</button>
           </div>
-        </form>
+        </div>
+      </div>
+      </form>
 
       </div>
     </div>
