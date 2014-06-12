ac66a05f031da03a34d0933e6ca90abd957a5242
starcalm96 <xingguang96>
2014-06-12 23:39:39 +0800

    把js都抽出来了
    课程查询的js

---
+++ js/scripts.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){
+
+  var myFunction = function(data){
+          var keyword = $("#courseKeyword").val();
+          switch (data["res"]) {
+            case "none":
+              $("#res").show();
+              $("#prompt").html("No query conditions!");
+              $("#table").hide();
+              $(".old").empty();
+              if (!keyword) { $("#res").hide() };
+              break;
+            case "noSeltype":
+              $("#prompt").html("No query type!\nPlease choose one query type!");
+              $("#table").hide();
+              $("#res").show();
+              $(".old").empty();
+              if (!keyword) { $("#res").hide() };
+              break;
+            case "fail":
+              $("#res").show();
+              $("#prompt").html("No records!");
+              $("#table").hide();
+              $(".old").empty();
+              if (!keyword) { $("#res").hide() };
+              break;
+            case "noKeyword":
+              $("#res").show();
+              $("#prompt").html("No keyword!\nPlease input some keywords!");
+              $("#table").hide();
+              $(".old").empty();
+              if (!keyword) { $("#res").hide() };
+              break;
+            default:
+              $(".old").empty();
+              $.each(data, function(row){
+                var newrow = document.createElement("tr");
+                var rowData = data[row];
+                $(newrow).addClass("old");
+                var result = "";
+                for (var i = 0; i < 4; i++) {
+                  result += "<td align='center'><small>" + rowData[i] + "</small></td>\n";
+                };
+                result += "<td align='center'><a type='button' class='btn btn-sm btn-default' href='course_info.php?course_id=" + rowData[0] + "'>More</a></td>\n";
+                result += "<td align='center'><a type='button' class='btn btn-sm btn-default' href=''>Delete</a></td>\n";
+                $(newrow).append(result);
+                $(newrow).insertAfter( $("#tableHead") );
+                if (keyword) { 
+                  $("#res").show(); 
+                  $("#prompt").hide();
+                  $("#table").show();
+                }else{
+                  $("#res").hide();
+                }
+              }); //end foreach
+          }; //end switch
+        }
+
+  $("#courseKeyword").keyup(function(){
+    var seltype = $("#seltype").val();
+    var keyword = $(this).val();
+    var order = $("#order").val();
+
+    $.ajax({
+      type:"POST",
+      url: "course_select_php.php",
+      dataType: "json",
+      data: "seltype=" + seltype + "&order=" + order + "&keyword=" + keyword,
+      success: myFunction, //end success function  
+    }) //end ajax
+  }) // end keyup
+  
+})
--- user_select.php
+++ user_select.php
@@ -26,105 +26,38 @@ $data = mysqli_query($dbc,$query);
       <!-- Begin page content -->
       <div class="container">
         <div class="page-header">
-          <h1> Course Information <small></small></h1>      
+          <h1> User Query <small></small></h1>      
         </div>
         <div class="panel panel-default">
           <div class="panel-heading">
           <p></p>
-            <form method="get" action="course_select.php">
-                  <div class="input-group">
-                    <span class="input-group-addon">
-                      <select name="seltype" id="search_menu" class="selectpicker">
-                        <option value="">Sort By</option>
-                        <option value="all">All</option>
-                        <option value="cid">Course ID</option>
-                        <option value="cname">Course Name</option>
-                        <option value="dept">Department</option>
-                        <option value="credit">Credit</option>
-                      </select>
-                      </span>
-                      <span class="input-group-addon">
-                      <select name="order">
-                        <option value="">Order By</option>
-                        <option value="all">All</option>
-                        <option value="cid">Course ID</option>
-                        <option value="cname">Course Name</option>
-                        <option value="dept">Department</option>
-                        <option value="credit">Credit</option>
-                      </select>
-                    </span>
-                    <span>
-                    <input type="text" id="input_1" placeholder="Keyword" class="form-control" name="keyword">
-                    </span>
-
-                  <!-- <div class="input-group-inline" id="input_2">
-                    <div class="row clearfix">
-                      <div class="col-md-4 column">
-                        <input id="limit_down" type="text" placeholder="Lower Bound" class="form-control" name ="lower_bound">
-                      </div>
-                      <div class="col-md-4 column">
-                        <input id="limit_up"  type="text" placeholder="Upper Bound" class="form-control" name ="upper_bound">
-                      </div>
-                      <div class="col-md-4 column">
-                       <input type="text" id="input_1" placeholder="Keyword" class="form-control" name="keyword">
-                      </div>
-                    </div>
-                  </div> -->
-                  <span class="input-group-btn">
-                    <button type="submit" class="btn btn-primary" type="button" name="submit">Search</button>
-                  </span>
+            <form>
+              <div class="input-group">
+                <span class="input-group-addon">
+                  <select name="seltype" class="selectpicker">
+                    <option value="">Sort By</option>
+                    <option value="cid">Course ID</option>
+                    <option value="cname">Course Name</option>
+                    <option value="dept">Department</option>
+                    <option value="credit">Credit</option>
+                  </select>
+                </span>
+                <span class="input-group-addon">
+                  <select name="order">
+                    <option value="">Order By</option>
+                    <option value="cid">Course ID</option>
+                    <option value="cname">Course Name</option>
+                    <option value="dept">Department</option>
+                    <option value="credit">Credit</option>
+                  </select>
+                </span>
+                <span>
+                  <input type="text" id="input_1" placeholder="Keyword" class="form-control" name="keyword">
+                </span>
+              </div> 
             </form>
-          </div> 
-          </div>        
-            <?php
-              if(isset($_GET['submit']))
-              {
-                echo'<h4 align="center">Search Resault</h4>';
-                $dbc = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
-                $seltype=$_GET["seltype"];
-                $keyword=$_GET["keyword"];
-                $up=$_GET["lower_bound"];
-                $down=$_GET["upper_bound"];
-                $order=$_GET["order"];
-                if($seltype=="all")
-                  $sql="SELECT * FROM course_info;";
+          </div>  
                 
-                else{
-                  if($up)
-                     $sql = "SELECT * FROM course_info WHERE $seltype>=$lower_bound AND $seltype <= $upper_bound ORDER BY $order;";
-                  else {
-                     $sql = "SELECT * FROM courst_info WHERE $seltype LIKE '%$keyword%' ORDER BY $order;";
-                  }
-                } //筛选排序功能有问题的！from QX
-                
-                $arr=mysqli_query($dbc,$sql);
-                if($arr){
-                  echo '<table class="table table-striped">';
-                  echo '<tr>';
-                  echo  "<td align='center'><small> Course ID </small></td>";
-                  echo  "<td align='center'><small> Course Name </small></td>";
-                  echo  "<td align='center'><small> Department </small></td>";
-                  echo  "<td align='center'><small> Credit </small></td>";
-                  echo  "<td align='center'><small> Description </small></td>";
-                  echo  "<td align='center'colspan='3'><small> Action </small></td>";
-                  echo '</tr>';
-                }
-                while($val=mysqli_fetch_row($arr)){
-                   echo "<tr>";
-                for($i=0;$i<count($val);$i++){
-                        echo "<td align='center'><small>".$val[$i]."</small></td>";
-                   }
-                   echo "<td align='right'><a type='button' class='btn btn-sm btn-default' href='course_info.php?course_id=".$val[0]."'>More</a></td>";
-                   echo "<td align='left'><a type='button' class='btn btn-sm btn-default' href=''>Delete</a></td>";
-
-                 
-                echo "</tr>";
-                
-              }
-              echo "</table>";
-            }
-            ?>
-          <!-- </div> -->
         </div>
       </div>
     </div> 
