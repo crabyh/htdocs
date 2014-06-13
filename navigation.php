f2c4e2fee09b4f828c932ade939aabef54501862
starcalm96 <xingguang96>
2014-06-13 19:30:51 +0800

    修改导航栏的标题

--- navigation.php
+++ navigation.php
@@ -30,7 +30,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="index.php">Education Sever System</a>
+      <a class="navbar-brand" href="index.php">Education Service System</a>
     </div>
     <div class="collapse navbar-collapse">
       <ul class="nav navbar-nav">
